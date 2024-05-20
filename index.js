24658e3dcf1616c904a27aef9372d75561884f1b
Aztro-dev <davidperezcastellanos@gmail.com>
2024-05-20 16:21:11 -0500

    Add the silly sandbox stuff to index.js

--- index.js
+++ index.js
@@ -4,33 +4,43 @@ const path = require("path");
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "html/index.html"));
-})
+  res.sendFile(path.join(__dirname, "html/index.html"));
+});
 
 app.get("/about", (req, res) => {
-    res.sendFile(path.join(__dirname, "html/about.html"))
-})
+  res.sendFile(path.join(__dirname, "html/about.html"));
+});
 
 app.get("/number-sense", (req, res) => {
-    res.sendFile(path.join(__dirname, "html/events/number-sense/number-sense.html"))
-})
+  res.sendFile(
+    path.join(__dirname, "html/events/number-sense/number-sense.html"),
+  );
+});
 app.get("/calculator", (req, res) => {
-    res.sendFile(path.join(__dirname, "html/events/calculator.html"))
-})
+  res.sendFile(path.join(__dirname, "html/events/calculator.html"));
+});
 app.get("/mathematics", (req, res) => {
-    res.sendFile(path.join(__dirname, "html/events/mathematics.html"))
-})
+  res.sendFile(path.join(__dirname, "html/events/mathematics.html"));
+});
 app.get("/science", (req, res) => {
-    res.sendFile(path.join(__dirname, "html/events/science.html"))
-})
+  res.sendFile(path.join(__dirname, "html/events/science.html"));
+});
 app.get("/computer-science", (req, res) => {
-    res.sendFile(path.join(__dirname, "html/events/computer-science.html"))
-})
+  res.sendFile(path.join(__dirname, "html/events/computer-science.html"));
+});
+app.get("/sandbox", (req, res) => {
+  res.sendFile(path.join(__dirname, "sandbox.html"));
+});
 
+app.get("/sandbox-script", (req, res) => {
+  res.sendFile(path.join(__dirname + "/sandbox.js"));
+});
 
 app.get("/number-sense/memorization/squares", (req, res) => {
-    res.sendFile(path.join(__dirname, "html/events/number-sense/memorization/squares.html"))
-})
+  res.sendFile(
+    path.join(__dirname, "html/events/number-sense/memorization/squares.html"),
+  );
+});
 
 //host folders
 app.use("/images", express.static(path.join(__dirname, "public/images")));
@@ -38,5 +48,5 @@ app.use("/css", express.static(path.join(__dirname, "css")));
 app.use("/js", express.static(path.join(__dirname, "js")));
 
 app.listen(PORT, () => {
-    console.log(`Server started on ${PORT}`)
-})
+  console.log(`Server started on ${PORT}`);
+});
